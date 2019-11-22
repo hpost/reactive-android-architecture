e590c787e89d39a0bbd2af8ab03352841c1b49da
Henning Post <henningpost@gmail.com>
2019-11-22 13:57:39 -0800

    Add unit tests for SearchApi

--- api/src/test/java/cc/femto/architecture/reactive/api/SearchApiSpec.kt
+++ api/src/test/java/cc/femto/architecture/reactive/api/SearchApiSpec.kt
@@ -1,16 +1,81 @@
 package cc.femto.architecture.reactive.api
 
+import cc.femto.architecture.reactive.api.model.Repository
+import cc.femto.architecture.reactive.api.model.SearchOrder
+import cc.femto.architecture.reactive.api.model.SearchRepositoriesResponse
+import cc.femto.architecture.reactive.api.model.SearchSort
 import cc.femto.spek.extras.rxGroup
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import kotlin.test.assertEquals
+import retrofit2.Response
+import retrofit2.adapter.rxjava2.Result
+import java.util.concurrent.TimeoutException
 
 class SearchApiSpec : Spek({
 
+    lateinit var service: SearchService
+    lateinit var observer: TestObserver<SearchRepositoriesEvent>
+    lateinit var searchApi: SearchApi
+
+    val query = "rx-mvi"
+    val sort = SearchSort.STARS
+    val order = SearchOrder.DESC
+
+    val repositories = listOf(
+        Repository(name = "repository-1", html_url = "url/1", stargazers_count = 42),
+        Repository(name = "repository-2", html_url = "url/2", stargazers_count = 1)
+    )
+    val successResponse = Response.success(
+        SearchRepositoriesResponse(repositories.size, repositories)
+    )
+    val successResult = Result.response(successResponse)
+    val errorResult = Result.error<SearchRepositoriesResponse>(TimeoutException("took too long"))
+
+    val progressEvent = SearchRepositoriesEvent.InProgress
+    val successEvent = SearchRepositoriesEvent.Success(
+        successResult.response()?.body()?.items.orEmpty()
+    )
+    val errorEvent = SearchRepositoriesEvent.Error(RuntimeException(errorResult.error()))
+
+    beforeEachTest {
+        service = mock {
+            on { searchRepositories(query, sort, order) } doReturn Observable.just(successResult)
+        }
+        searchApi = SearchApi(service)
+    }
+
     rxGroup("SearchApi") {
-        describe("adding two numbers") {
-            it("should yield the sum") {
-                assertEquals(7, 3 + 4)
+        describe("search repositories") {
+            context("success") {
+                beforeEachTest {
+                    observer = searchApi.searchRepositories(query, sort, order).test()
+                }
+                it("emits correct events") {
+                    observer.assertValues(
+                        progressEvent,
+                        successEvent
+                    )
+                }
+            }
+            context("error") {
+                beforeEachTest {
+                    whenever(service.searchRepositories(any(), any(), any()))
+                        .doReturn(Observable.just(errorResult))
+                    observer = searchApi.searchRepositories(query, sort, order).test()
+                }
+                it("emits correct events") {
+                    observer.assertValueAt(0, progressEvent)
+                    observer.assertValueAt(1) {
+                        val message = (it as SearchRepositoriesEvent.Error).error?.message
+                        message == errorEvent.error?.message
+                    }
+                }
             }
         }
     }
