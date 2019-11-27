9b9ccc3917aa37973888b81122acd29ef3db89a5
Henning Post <henningpost@gmail.com>
2019-11-27 09:56:39 -0800

    Use TestScheduler in SearchApiSpec to speed up error retry behavior

--- api/src/test/java/cc/femto/architecture/reactive/api/SearchApiSpec.kt
+++ api/src/test/java/cc/femto/architecture/reactive/api/SearchApiSpec.kt
@@ -4,6 +4,7 @@ import cc.femto.architecture.reactive.api.model.Repository
 import cc.femto.architecture.reactive.api.model.SearchOrder
 import cc.femto.architecture.reactive.api.model.SearchRepositoriesResponse
 import cc.femto.architecture.reactive.api.model.SearchSort
+import cc.femto.kommon.extensions.pow
 import cc.femto.spek.extras.rxGroup
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
@@ -11,14 +12,17 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.TestScheduler
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import retrofit2.Response
 import retrofit2.adapter.rxjava2.Result
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
 class SearchApiSpec : Spek({
 
+    val testScheduler = TestScheduler()
     lateinit var service: SearchService
     lateinit var observer: TestObserver<SearchRepositoriesEvent>
     lateinit var searchApi: SearchApi
@@ -41,7 +45,7 @@ class SearchApiSpec : Spek({
     val successEvent = SearchRepositoriesEvent.Success(
         successResult.response()?.body()?.items.orEmpty()
     )
-    val errorEvent = SearchRepositoriesEvent.Error(RuntimeException(errorResult.error()))
+    val errorEvent = SearchRepositoriesEvent.Error(errorResult.error())
 
     beforeEachTest {
         service = mock {
@@ -50,7 +54,7 @@ class SearchApiSpec : Spek({
         searchApi = SearchApi(service)
     }
 
-    rxGroup("SearchApi") {
+    rxGroup("SearchApi", scheduler = testScheduler) {
         describe("search repositories") {
             context("success") {
                 beforeEachTest {
@@ -68,6 +72,8 @@ class SearchApiSpec : Spek({
                     whenever(service.searchRepositories(any(), any(), any()))
                         .doReturn(Observable.just(errorResult))
                     observer = searchApi.searchRepositories(query, sort, order).test()
+                    // account for exponential backoff retry behavior
+                    testScheduler.advanceTimeBy(2.pow(3).toLong(), TimeUnit.SECONDS)
                 }
                 it("emits correct events") {
                     observer.assertValueAt(0, progressEvent)
