c2341957987f2efbe308b100f812df6612d72f7a
Henning Post <henningpost@gmail.com>
2019-12-05 00:50:44 -0800

    Add ReyclerView and String extensions

---
+++ common/src/main/java/cc/femto/android/common/util/RecyclerViewExtensions.kt
@@ -0,0 +1,17 @@
+package cc.femto.android.common.util
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jakewharton.rxbinding3.view.scrollChangeEvents
+import io.reactivex.Observable
+
+fun RecyclerView.pageLoadEvents(itemThreshold: Int = 10): Observable<Boolean> =
+    this.scrollChangeEvents()
+        .map {
+            val totalItemCount = this.layoutManager?.itemCount ?: 0
+            val lastVisibleItem = (this.layoutManager as? LinearLayoutManager)
+                ?.findLastVisibleItemPosition() ?: 0
+            lastVisibleItem + itemThreshold > totalItemCount
+        }
+        .distinctUntilChanged()
+        .filter { shouldLoadPage -> shouldLoadPage }
---
+++ common/src/main/java/cc/femto/android/common/util/StringExtensions.kt
@@ -0,0 +1,14 @@
+package cc.femto.android.common.util
+
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+
+@Suppress("DEPRECATION")
+fun String.toHtmlSpan(): Spanned {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        Html.fromHtml(this)
+    }
+}
