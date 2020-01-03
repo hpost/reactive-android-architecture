70c2a38364acda4817d83bc006f358de25f0f0d3
Henning Post <henningpost@gmail.com>
2020-01-02 17:56:31 -0800

    Trim spans parsed from HTML to eliminate extra newlines
    See https://stackoverflow.com/questions/16585557/extra-padding-on-textview-with-html-contents#16745540

--- common/src/main/java/cc/femto/android/common/util/StringExtensions.kt
+++ common/src/main/java/cc/femto/android/common/util/StringExtensions.kt
@@ -2,13 +2,12 @@ package cc.femto.android.common.util
 
 import android.os.Build
 import android.text.Html
-import android.text.Spanned
 
 @Suppress("DEPRECATION")
-fun String.toHtmlSpan(): Spanned {
+fun String.toHtmlSpan(): CharSequence {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).trim()
     } else {
-        Html.fromHtml(this)
+        Html.fromHtml(this).trim()
     }
 }
